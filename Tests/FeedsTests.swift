4f8fee9bf9539cbd3c49baa86db2e725c499c341
danielmoro <daniel.moro@me.com>
2021-02-14 08:03:22 +0100

    Maek load function on the loader that feteches from given URL

--- Tests/FeedsTests.swift
+++ Tests/FeedsTests.swift
@@ -7,17 +7,45 @@
 
 import XCTest
 
-class RemoteFeedLoader {}
+class RemoteFeedLoader {
+    init(url: URL, client: HTTPClient) {
+        self.client = client
+        self.url = url
+    }
+    
+    let client: HTTPClient
+    let url: URL
+    
+    func load() {
+        client.get(from: url)
+    }
+}
 
 class HTTPClient {
-    var remoteFeedURL: URL?
+    var requestedURL: URL?
+    
+    func get(from url: URL) {
+        requestedURL = url
+    }
 }
 
 class FeedsTests: XCTestCase {
+    
     func test_init_doesNotFetchURL() {
-        _ = RemoteFeedLoader()
+        let url = URL(string: "http://a-url.com")!
         let client = HTTPClient()
+        _ = RemoteFeedLoader(url: url, client: client)
 
-        XCTAssertNil(client.remoteFeedURL)
+        XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_fetchesProperURL() {
+        let url = URL(string: "http://a-url.com")!
+        let client = HTTPClient()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
 }
