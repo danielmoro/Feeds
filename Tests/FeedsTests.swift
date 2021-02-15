df322b58c41a8671f906c4e9f5f2239f92f5f4fe
danielmoro <daniel.moro@me.com>
2021-02-15 09:46:06 +0100

    Improve ClientSpy with convinient function for expectation

--- Tests/FeedsTests.swift
+++ Tests/FeedsTests.swift
@@ -45,6 +45,7 @@ class FeedsTests: XCTestCase {
             resultError = error
         })
 
+        client.expectToComplete(with: expectedError)
         client.completions[0](expectedError)
 
         XCTAssertEqual(resultError, RemoteFeedLoader.Error.connectivity)
@@ -66,6 +67,10 @@ class FeedsTests: XCTestCase {
         func get(from url: URL, completion: @escaping (Error?) -> Void) {
             completions.append(completion)
             requestedURLs.append(url)
+        }
+
+        func expectToComplete(with error: Error?, at index: Int = 0) {
+            completions[index](error)
         }
     }
 }
