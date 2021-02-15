3be25b86e4d4bf7e18001094abbffa3e10f545b2
danielmoro <daniel.moro@me.com>
2021-02-15 10:47:18 +0100

    Introduce HTTPClientResult to better represent load completion

--- Tests/FeedsTests.swift
+++ Tests/FeedsTests.swift
@@ -78,18 +78,18 @@ class FeedsTests: XCTestCase {
     }
 
     private class HTTPClientSpy: HTTPClient {
-        var messages: [(url: URL, completion: (Error?, HTTPURLResponse?) -> Void)] = []
+        var messages: [(url: URL, completion: (HTTPClientResult) -> Void)] = []
 
         var requestedURLs: [URL] {
             messages.map(\.url)
         }
 
-        func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
+        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
             messages.append((url, completion))
         }
 
-        func complete(with error: Error?, at index: Int = 0) {
-            messages[index].completion(error, nil)
+        func complete(with error: Error, at index: Int = 0) {
+            messages[index].completion(.failure(error))
         }
 
         func complete(withStatusCode code: Int, at index: Int = 0) {
@@ -98,8 +98,8 @@ class FeedsTests: XCTestCase {
                 statusCode: code,
                 httpVersion: nil,
                 headerFields: nil
-            )
-            messages[index].completion(nil, response)
+            )!
+            messages[index].completion(.success(response))
         }
     }
 }
