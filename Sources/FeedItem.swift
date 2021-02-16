5a03e64e464676d71f07fc6c3e031961b287c985
danielmoro <daniel.moro@me.com>
2021-02-16 08:44:28 +0100

    Publish init and properties of FeddItem

--- Sources/FeedItem.swift
+++ Sources/FeedItem.swift
@@ -6,8 +6,15 @@
 import Foundation
 
 public struct FeedItem: Equatable {
-    let id: UUID // swiftlint:disable:this identifier_name
-    let description: String?
-    let location: String
-    let imageURL: URL?
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+
+    public let id: UUID // swiftlint:disable:this identifier_name
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
 }
