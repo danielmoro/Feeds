b77e6299eb24f47ca9950a32bd3206e327cde45c
Daniel Moro <daniel.moro@me.com>
2021-02-20 09:28:23 +0100

    Cleanup test code as recommended by a linter

--- Tests/FeedsTests.swift
+++ Tests/FeedsTests.swift
@@ -190,7 +190,10 @@ class FeedsTests: XCTestCase {
             switch (receivedResult, expectedResult) {
             case let (.success(receivedItems), .success(expectedItems)):
                 XCTAssertEqual(receivedItems, expectedItems, file: file, line: line)
-            case let (.failure(receivedError as RemoteFeedLoader.Error), .failure(expectedError as RemoteFeedLoader.Error)):
+            case let (
+                .failure(receivedError as RemoteFeedLoader.Error),
+                .failure(expectedError as RemoteFeedLoader.Error)
+            ):
                 XCTAssertEqual(receivedError, expectedError, file: file, line: line)
             default:
                 XCTFail("Expected \(expectedResult) received \(receivedResult)", file: file, line: line)
