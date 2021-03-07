2ecda6aec850f991ef1d5d1140d6e6e2dc1b3b09
danielmoro <daniel.moro@me.com>
2021-03-07 09:06:06 +0100

    Add memory leak tests for system under test

--- Tests/Feed Cache/CacheFeedUseCaseTests.swift
+++ Tests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -44,10 +44,11 @@ class CacheFeedUseCaseTests: XCTestCase {
 
     // MARK: - Helpers
 
-    private func makeSUT() -> (sut: LocalFeedLoader, store: FeedStore) {
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStore) {
         let store = FeedStore()
         let sut = LocalFeedLoader(store: store)
-
+        trackMemoryLeaks(sut, file: file, line: line)
+        trackMemoryLeaks(store, file: file, line: line)
         return (sut: sut, store: store)
     }
 
