be03644591bd49cab68b6c4e152ef20a7fb612fe
Daniel Moro <daniel.moro@me.com>
2021-03-13 08:52:17 +0100

    Add `RemoteFeedItem` data transfer representation to decouple remote feed api from feed feature

--- Sources/Feed API/FeedItemsMapper.swift
+++ Sources/Feed API/FeedItemsMapper.swift
@@ -5,35 +5,30 @@
 
 import Foundation
 
+internal struct RemoteFeedItem: Decodable {
+    internal let id: UUID // swiftlint:disable:this identifier_name
+    internal let description: String?
+    internal let location: String?
+    internal let image: URL
+}
+
 internal enum FeedItemsMapper {
     private struct Root: Decodable {
-        var images: [Item]
-    }
-
-    private struct Item: Decodable {
-        private let id: UUID // swiftlint:disable:this identifier_name
-        private let description: String?
-        private let location: String?
-        private let image: URL
-
-        var item: FeedItem {
-            FeedItem(id: id, description: description, location: location, imageURL: image)
-        }
+        var images: [RemoteFeedItem]
     }
 
     private static let OK_200 = 200 // swiftlint:disable:this identifier_name
 
-    internal static func map(data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
+    internal static func map(data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
         guard response.statusCode == OK_200 else {
-            return .failure(RemoteFeedLoader.Error.invalidData)
+            throw RemoteFeedLoader.Error.invalidData
         }
 
         do {
             let root = try JSONDecoder().decode(Root.self, from: data)
-            let items = root.images.map(\.item)
-            return .success(items)
+            return root.images
         } catch {
-            return .failure(RemoteFeedLoader.Error.invalidData)
+            throw RemoteFeedLoader.Error.invalidData
         }
     }
 }
