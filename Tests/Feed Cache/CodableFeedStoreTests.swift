969dc1f680f6958266bb8f24545a518cf52824f0
Daniel Moro <daniel.moro@me.com>
2021-03-20 12:56:10 +0100

    `CodableFeedStore` retreive returns empty result when cache is empty

---
+++ Tests/Feed Cache/CodableFeedStoreTests.swift
@@ -0,0 +1,35 @@
+//
+//  CodableFeedStoreTests.swift
+//  FeedsTests
+//
+//  Created by Daniel Moro on 20.3.21..
+//
+
+import Feeds
+import Foundation
+import XCTest
+
+class CodableFeedStore {
+    func retreive(completion: @escaping FeedStore.RetreivalCompletion) {
+        completion(.empty)
+    }
+}
+
+class CodableFeedStoreTests: XCTestCase {
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        let sut = CodableFeedStore()
+
+        let exp = XCTestExpectation(description: "wait for retreival to complete")
+        sut.retreive { result in
+            switch result {
+            case .empty:
+                break
+            default:
+                XCTFail("Expected to receive empty result, got \(result) instead")
+            }
+            exp.fulfill()
+        }
+
+        wait(for: [exp], timeout: 1.0)
+    }
+}
