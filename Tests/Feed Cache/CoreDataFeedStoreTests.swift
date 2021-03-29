791833af44b3f1394f0693727334c387997206fb
danielmoro <daniel.moro@me.com>
2021-03-29 12:42:29 +0200

    Create new `FeedStore` implementation, one that uses `CoreData` as persistence framework.

---
+++ Tests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -0,0 +1,75 @@
+//
+//  Created by Daniel Moro on 29.3.21.
+//  Copyright © 2021 Daniel Moro. All rights reserved.
+//  
+
+import XCTest
+import Feeds
+
+class CoreDataFeedStore: FeedStore {
+    func deleteCacheFeed(completion: @escaping Completion) {
+        
+    }
+    
+    func insert(feed: [LocalFeedImage], timestamp: Date, completion: @escaping Completion) {
+        
+    }
+    
+    func retreive(completion: @escaping RetreivalCompletion) {
+        
+    }
+    
+    
+}
+
+class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+        
+    }
+    
+    func test_retrieve_deliversFoundValuesOnExistingCache() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnExistingCache() {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnEmptyCache() {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnNonEmptyCache() {
+        
+    }
+    
+    func test_insert_overridesPreviouslyInsertedCacheValues() {
+        
+    }
+    
+    func test_delete_deliversNoErrorOnEmptyCache() {
+        
+    }
+    
+    func test_delete_deliversNoErrorOnNonEmptyCache() {
+        
+    }
+    
+    func test_delete_hasNoSideEffectsOnEmptyCache() {
+        
+    }
+    
+    func test_delete_emptiesPreviouslyInsertedCache() {
+        
+    }
+    
+    func test_storeSideEffects_runSerially() {
+        
+    }
+    
+    
+}
