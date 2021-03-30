84c93c653aafefead1eedb4dc1c188f6438581ee
danielmoro <daniel.moro@me.com>
2021-03-30 11:17:36 +0200

    `LocalFeedLoader` in integration with `CoreDataFeedStore` loads images previously saved by another instance of loader

--- Tests/Integration/CoreDataFeedIntegrationTests.swift
+++ Tests/Integration/CoreDataFeedIntegrationTests.swift
@@ -26,49 +26,76 @@ class CoreDataFeedStoreTests: XCTestCase {
         XCTAssertEqual(receivedFeed, [])
     }
 
-    // MARK: - Helpers
+    func test_loadReturnsImagesSavedByAnotherStore() throws {
+        let sutToSave = try makeSUT()
+        let sutToLoad = try makeSUT()
+        let anyFeed = uniqueImageFeed().models
 
-    private func makeSUT() throws -> LocalFeedLoader {
-        let url = CoreDataFeedStoreTests.testSpecificStoreURL()
+        let saveExp = expectation(description: "Wait for save to complete")
+        sutToSave.save(anyFeed) { result in
+            XCTAssertNil(result, "Expected not to receive anything, got \(String(describing: result)) instead")
+
+            saveExp.fulfill()
+        }
+
+        wait(for: [saveExp], timeout: 1.0)
+
+        let loadExp = expectation(description: "wait for load to complete")
+        var receivedFeed: [FeedImage]?
+        sutToLoad.load { result in
+            switch result {
+            case let .success(feed):
+                receivedFeed = feed
+            default:
+                XCTFail("Expected success with empty cache, got \(result) instead")
+            }
+            loadExp.fulfill()
+        }
+
+        wait(for: [loadExp], timeout: 1.0)
+        XCTAssertEqual(receivedFeed, anyFeed)
+    }
+
     override func setUp() {
         super.setUp()
-        
+
         setupEmptyStoreState()
     }
-    
+
     override func tearDown() {
         super.tearDown()
-        
+
         undoTestSideEffects()
     }
-    
+
     private func setupEmptyStoreState() {
         cleanupCache()
     }
-    
+
     private func undoTestSideEffects() {
         cleanupCache()
     }
-    
-    //MARK: - Helpers
+
+    // MARK: - Helpers
+
     private func makeSUT(
-        file: StaticString = #filePath,
-        line: UInt = #line
+        file _: StaticString = #filePath,
+        line _: UInt = #line
     ) throws -> LocalFeedLoader {
         let url = testSpecificStoreURL()
         let store = try CoreDataFeedStore(url: url)
         let sut = LocalFeedLoader(store: store, currentDate: Date.init)
-        
+
         return sut
     }
-    
+
     private func testSpecificStoreURL() -> URL {
         let cachesURL = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
         return cachesURL
             .appendingPathComponent("\(type(of: self))-Store")
             .appendingPathExtension("sqlite")
     }
-    
+
     private func cleanupCache() {
         try? FileManager.default.removeItem(at: testSpecificStoreURL())
     }
