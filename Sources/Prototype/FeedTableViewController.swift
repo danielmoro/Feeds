ad9018e45a4cf57a50e9bb062656760cd27aa004
danielmoro <daniel.moro@me.com>
2021-03-31 13:46:53 +0200

    Add prototype content for demoing purposes

--- Sources/Prototype/FeedTableViewController.swift
+++ Sources/Prototype/FeedTableViewController.swift
@@ -1,20 +1,26 @@
 //
 //  Created by Daniel Moro on 31.3.21.
 //  Copyright © 2021 Daniel Moro. All rights reserved.
-//  
+//
 
 import UIKit
 
+struct FeedImageViewModel {
+    var description: String?
+    var location: String?
+    var imageName: String
+}
+
 class FeedViewController: UITableViewController {
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+    override func numberOfSections(in _: UITableView) -> Int {
+        1
     }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+
+    override func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
+        10
     }
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return tableView.dequeueReusableCell(withIdentifier: "FeedImageCell", for: indexPath)
+        tableView.dequeueReusableCell(withIdentifier: "FeedImageCell", for: indexPath)
     }
 }
