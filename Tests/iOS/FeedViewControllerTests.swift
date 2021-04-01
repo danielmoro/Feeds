1cb98d22df11a37d2a5983b4873bf1ac5e55085d
danielmoro <daniel.moro@me.com>
2021-04-01 14:04:48 +0200

    User initiated refresh loads feed

--- Tests/iOS/FeedViewControllerTests.swift
+++ Tests/iOS/FeedViewControllerTests.swift
@@ -7,7 +7,7 @@ import FeedsCore
 import UIKit
 import XCTest
 
-class FeedViewController: UIViewController {
+class FeedViewController: UITableViewController {
     private var loader: FeedLoader?
 
     convenience init(loader: FeedLoader) {
@@ -18,6 +18,13 @@ class FeedViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
+        load()
+    }
+
+    @objc
+    private func load() {
         loader?.load(completion: { _ in })
     }
 }
@@ -35,6 +42,19 @@ final class FeedViewControllerTests: XCTestCase {
         sut.loadViewIfNeeded()
 
         XCTAssertEqual(loader.loadRequestCount, 1)
+    }
+
+    func test_pullToRefresh_loadsFeed() {
+        let (sut, loader) = makeSUT()
+        sut.loadViewIfNeeded()
+
+        sut.refreshControl?.allTargets.forEach { target in
+            sut.refreshControl?.actions(forTarget: target, forControlEvent: .valueChanged)?.forEach {
+                (target as NSObject).perform(Selector($0))
+            }
+        }
+
+        XCTAssertEqual(loader.loadRequestCount, 2)
     }
 
     // MARK: - Helpers
