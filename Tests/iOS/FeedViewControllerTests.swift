59b512fcb5dc6e50ddaef79e9e759c7a64e4794e
danielmoro <daniel.moro@me.com>
2021-04-01 17:22:29 +0200

    Add clear expectation messages for each assert in one test

--- Tests/iOS/FeedViewControllerTests.swift
+++ Tests/iOS/FeedViewControllerTests.swift
@@ -36,32 +36,52 @@ final class FeedViewControllerTests: XCTestCase {
     func test_loadFeedActions_requestFeedLoad() {
         let (sut, loader) = makeSUT()
 
-        XCTAssertEqual(loader.completions.count, 0)
+        XCTAssertEqual(loader.completions.count, 0, "Expect no loading request before view is loaded")
 
         sut.loadViewIfNeeded()
-        XCTAssertEqual(loader.completions.count, 1)
+        XCTAssertEqual(loader.completions.count, 1, "Expect loading reqest once view is loaded")
 
         sut.simulateUserInitiatedFeedReload()
-        XCTAssertEqual(loader.completions.count, 2)
+        XCTAssertEqual(loader.completions.count, 2, "Expect another loading request after user initiated reload")
 
         sut.simulateUserInitiatedFeedReload()
-        XCTAssertEqual(loader.completions.count, 3)
+        XCTAssertEqual(
+            loader.completions.count,
+            3,
+            "Expect yet another loading request after another user initiated reload"
+        )
     }
 
     func test_loadFeedIndicator_isVisibleWhileLoadingFeed() {
         let (sut, loader) = makeSUT()
         sut.loadViewIfNeeded()
 
-        XCTAssertEqual(sut.isShowingLoadingIndicator, true)
+        XCTAssertEqual(
+            sut.isShowingLoadingIndicator,
+            true,
+            "Expect loading indicator to be visible once view is loaded"
+        )
 
         loader.complete(at: 0)
-        XCTAssertEqual(sut.isShowingLoadingIndicator, false)
+        XCTAssertEqual(
+            sut.isShowingLoadingIndicator,
+            false,
+            "Expect loading indicator no to be visible after load is complete"
+        )
 
         sut.simulateUserInitiatedFeedReload()
-        XCTAssertEqual(sut.isShowingLoadingIndicator, true)
+        XCTAssertEqual(
+            sut.isShowingLoadingIndicator,
+            true,
+            "Expect loading indicator to be visible user initiated reload"
+        )
 
         loader.complete(at: 1)
-        XCTAssertEqual(sut.isShowingLoadingIndicator, false)
+        XCTAssertEqual(
+            sut.isShowingLoadingIndicator,
+            false,
+            "Expect loading indicator note no to be visible after user initiated load is complete"
+        )
     }
 
     // MARK: - Helpers
