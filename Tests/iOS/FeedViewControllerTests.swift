f0a0b7eeffb207e6e2eb2e38bb8df26bd635775a
danielmoro <daniel.moro@me.com>
2021-04-11 10:38:28 +0200

    Moving test helper in to separate class

--- Tests/iOS/FeedViewControllerTests.swift
+++ Tests/iOS/FeedViewControllerTests.swift
@@ -247,61 +247,6 @@ final class FeedViewControllerTests: XCTestCase {
                        "Expected second image URL request cancelled once image is not near visible anymore")
     }
 
-    // MARK: - Helpers
-
-    class LoaderSpy: FeedLoader, FeedImageLoader {
-        // MARK: - FeedLoader
-
-        var feedRequests: [(FeedLoader.Result) -> Void] = []
-
-        func load(completion: @escaping (FeedLoader.Result) -> Void) {
-            feedRequests.append(completion)
-        }
-
-        func completeFeedLoading(with images: [FeedImage] = [], at index: Int) {
-            feedRequests[index](.success(images))
-        }
-
-        func completeFeedLoading(with error: Error, at index: Int) {
-            feedRequests[index](.failure(error))
-        }
-
-        // MARK: - FeedImageLoader
-
-        private class TaskSpy: FeedImageLoadTask {
-            init(cancelCallback: @escaping () -> Void) {
-                self.cancelCallback = cancelCallback
-            }
-
-            var cancelCallback: () -> Void
-            func cancel() {
-                cancelCallback()
-            }
-        }
-
-        var imageLoadRequests: [(url: URL, completion: (FeedImageResult) -> Void)] = []
-        var loadedImageURLs: [URL] {
-            imageLoadRequests.map(\.url)
-        }
-
-        var cancelledImageURLs: [URL] = []
-
-        func loadImageData(from url: URL, completion: @escaping (FeedImageResult) -> Void) -> FeedImageLoadTask {
-            imageLoadRequests.append((url, completion))
-            return TaskSpy { [weak self] in
-                self?.cancelledImageURLs.append(url)
-            }
-        }
-
-        func completeImageLoading(with data: Data = Data(), at index: Int) {
-            imageLoadRequests[index].completion(.success(data))
-        }
-
-        func completeImageLoadingWithError(at index: Int) {
-            imageLoadRequests[index].completion(.failure(anyNSError()))
-        }
-    }
-
     private func makeSUT(
         file: StaticString = #filePath,
         line: UInt = #line
@@ -385,5 +330,60 @@ final class FeedViewControllerTests: XCTestCase {
         images.enumerated().forEach { index, image in
             assertThat(sut, hasViewConfiguredFor: image, at: index)
         }
+    }
+}
+
+// MARK: - Helpers
+
+class LoaderSpy: FeedLoader, FeedImageLoader {
+    // MARK: - FeedLoader
+
+    var feedRequests: [(FeedLoader.Result) -> Void] = []
+
+    func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        feedRequests.append(completion)
+    }
+
+    func completeFeedLoading(with images: [FeedImage] = [], at index: Int) {
+        feedRequests[index](.success(images))
+    }
+
+    func completeFeedLoading(with error: Error, at index: Int) {
+        feedRequests[index](.failure(error))
+    }
+
+    // MARK: - FeedImageLoader
+
+    private class TaskSpy: FeedImageLoadTask {
+        init(cancelCallback: @escaping () -> Void) {
+            self.cancelCallback = cancelCallback
+        }
+
+        var cancelCallback: () -> Void
+        func cancel() {
+            cancelCallback()
+        }
+    }
+
+    var imageLoadRequests: [(url: URL, completion: (FeedImageResult) -> Void)] = []
+    var loadedImageURLs: [URL] {
+        imageLoadRequests.map(\.url)
+    }
+
+    var cancelledImageURLs: [URL] = []
+
+    func loadImageData(from url: URL, completion: @escaping (FeedImageResult) -> Void) -> FeedImageLoadTask {
+        imageLoadRequests.append((url, completion))
+        return TaskSpy { [weak self] in
+            self?.cancelledImageURLs.append(url)
+        }
+    }
+
+    func completeImageLoading(with data: Data = Data(), at index: Int) {
+        imageLoadRequests[index].completion(.success(data))
+    }
+
+    func completeImageLoadingWithError(at index: Int) {
+        imageLoadRequests[index].completion(.failure(anyNSError()))
     }
 }
